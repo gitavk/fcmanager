ec14b8b668fe909cec3ffdcbd8ac07f383b3bd9f
alex <3apa3ka3@gmail.com>
2017-04-16 14:02:52 +0300

    Online: Open session with cashier.

--- windows/emul/cgi-bin/cashier.py
+++ windows/emul/cgi-bin/cashier.py
@@ -3,11 +3,13 @@
 import os
 import subprocess
 import urllib2
+import ConfigParser
 import time
-import cgi, cgitb
+import cgi
+import cgitb
 
-print "Content-type: text/html\n\n";
-print "\n";
+print "Content-type: text/html\n\n"
+print "\n"
 
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
@@ -15,24 +17,34 @@ form = cgi.FieldStorage()
 # Get data from fields
 action = form.getvalue('action')
 callback = form.getvalue('callback')
+cashier = form.getvalue('cashier')
 
 if action == 'session':
-        #check date
-        tdelta = os.popen("echo 'CheckDate\n' >> cashier.log").read()
-        if tdelta > 60:
-                r = os.popen("echo 'logSyncDate\n' >> cashier.log").read()
-        p = os.popen("echo 'OpenSession\n' >> cashier.log").read()
+    config = ConfigParser.ConfigParser()
+    config.read("config.ini")
+    # lets create that config file for next time...
+    cfgfile = open("config.ini",'w')
+    # add the settings to the structure of the file, and lets write it out...
+    config.set('var','cash_user',cashier)
+    config.write(cfgfile)
+    cfgfile.close()
+    # check date
+    tdelta = os.popen("echo 'CheckDate\n' >> cashier.log").read()
+    if tdelta > 60:
+        r = os.popen("echo 'logSyncDate\n' >> cashier.log").read()
+    p = os.popen("echo 'OpenSession\n' >> cashier.log").read()
 elif action == 'z':
-        # Отчет по отделам
-        # p = subprocess.Popen("echo 'PrintDepartmentReport\n' >> cashier.log", shell=True, stdout=subprocess.PIPE)
-        p = subprocess.Popen("cat /home/avk/old/user.bak.tar.bz2 > dump", shell=True, stdout=subprocess.PIPE)
-        p.wait()
-        # Отчет с гашением
-        p = os.popen("echo 'PrintReportWithCleaning\n' >> cashier.log").read()
+    # Отчет по отделам
+    # p = subprocess.Popen("echo 'PrintDepartmentReport\n' >> cashier.log", shell=True, stdout=subprocess.PIPE)
+    p = subprocess.Popen(
+        "cat /home/avk/old/user.bak.tar.bz2 > dump", shell=True, stdout=subprocess.PIPE)
+    p.wait()
+    # Отчет с гашением
+    p = os.popen("echo 'PrintReportWithCleaning\n' >> cashier.log").read()
 elif action == 'x':
-        # Отчет без гашения
-        p = os.popen("echo 'PrintReportWithoutCleaning\n' >> cashier.log").read()
+    # Отчет без гашения
+    p = os.popen("echo 'PrintReportWithoutCleaning\n' >> cashier.log").read()
 else:
-        p = '-1000'
+    p = '-1000'
 
-print  '%s({"res" : "%s"})' % (callback, p)
+print '%s({"res" : "%s"})' % (callback, p)
