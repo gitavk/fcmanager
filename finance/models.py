09739319951e06cda3d8d4d34698feff35ee1d62
Alexey <gitavk@users.noreply.github.com>
2017-08-19 13:06:45 +0300

    Update models.py
    fix typo in return

--- finance/models.py
+++ finance/models.py
@@ -142,7 +142,7 @@ class Goods(models.Model):
             Q(date_start__lte=date), Q(goods=self),
             (Q(date_end__isnull=True) | Q(date_end__gt=date)),
         )
-        rerurn prices[0].value if prices else 0
+        return prices[0].value if prices else 0
 
     def price(self):
         try:
