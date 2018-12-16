912e301f02c6c68420426030e13cab04cc6e7d32
avk <avk@alarstudios.com>
2018-12-16 21:28:56 +0100

    Added PeriodTimeType to the admin panel.

--- contract/admin.py
+++ contract/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Contract
+from .models import Contract, PeriodTimeType, PeriodTime
 
 
+class PeriodTimeInline(admin.TabularInline):
+    model = PeriodTime
+
+
+class PeriodTimeTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        PeriodTimeInline,
+    ]
+
+
+admin.site.register(PeriodTimeType, PeriodTimeTypeAdmin)
 admin.site.register(Contract)
