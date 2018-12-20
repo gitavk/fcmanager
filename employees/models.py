d6c0b680011c67b2c1b3a49d66155d95869a0de2
avk <avk@alarstudios.com>
2018-12-20 23:59:39 +0100

    Refactor PEP8 employees/models.py.

--- employees/models.py
+++ employees/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from datetime import date, datetime
+from datetime import datetime
 
 from django.db import models
 from django.conf import settings
+
 
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
@@ -15,9 +16,13 @@ class Employee(models.Model):
     address = models.CharField(max_length=255, blank=True, null=True,)
     # 9223372036854775807
     card = models.BigIntegerField(blank=True, null=True, unique=True)
-    login = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT
-                            , blank=True, null=True,)
-    note = models.CharField(max_length=200,blank=True, null=True)
+    login = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    note = models.CharField(max_length=200, blank=True, null=True)
 
     def is_online(self):
         if Visits.objects.filter(employee=self, date_end__isnull=True):
@@ -37,10 +42,10 @@ class Employee(models.Model):
         unique_together = ('firstname', 'lastname', 'patronymic', 'born')
 
     def save(self, *args, **kwargs):
-            self.firstname = self.firstname.upper()
-            self.lastname = self.lastname.upper()
-            self.patronymic = self.patronymic.upper()
-            super(Employee, self).save(*args, **kwargs)
+        self.firstname = self.firstname.upper()
+        self.lastname = self.lastname.upper()
+        self.patronymic = self.patronymic.upper()
+        super(Employee, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.get_full_name()
@@ -49,9 +54,15 @@ class Employee(models.Model):
         return unicode('%s %s' % (self.firstname, self.lastname))
 
     def initials(self):
-        return unicode('%s%s %s. %s.' % 
-                        (self.lastname[0], self.lastname[1:].lower(), 
-                         self.firstname[0], self.patronymic[0]))
+        return unicode(
+            '%s%s %s. %s.' %
+            (
+                self.lastname[0],
+                self.lastname[1:].lower(),
+                self.firstname[0],
+                self.patronymic[0]
+            )
+        )
 
     def whistory(self):
         return WorkRecord.objects.filter(employee=self).order_by('date_start')
@@ -62,11 +73,13 @@ class Employee(models.Model):
         except WorkRecord.DoesNotExist:
             return 0
 
+
 class Visits(models.Model):
     id = models.AutoField(primary_key=True)
     date_start = models.DateTimeField()
     date_end = models.DateTimeField(blank=True, null=True)
     employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
+
 
 class Department(models.Model):
     id = models.AutoField(primary_key=True)
@@ -79,6 +92,7 @@ class Department(models.Model):
         self.name = self.name.upper()
         super(Department, self).save(*args, **kwargs)
 
+
 class Position(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, unique=True,)
@@ -90,6 +104,7 @@ class Position(models.Model):
         self.name = self.name.upper()
         super(Position, self).save(*args, **kwargs)
 
+
 class DepartmentPosition(models.Model):
     id = models.AutoField(primary_key=True)
     position = models.ForeignKey(Position, on_delete=models.PROTECT)
@@ -98,12 +113,14 @@ class DepartmentPosition(models.Model):
     class Meta:
         unique_together = ('position', 'department')
 
+
 class WorkRecord(models.Model):
     id = models.AutoField(primary_key=True)
     date_start = models.DateField()
     date_end = models.DateField(blank=True, null=True)
     employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
     wposition = models.ForeignKey(DepartmentPosition, on_delete=models.PROTECT)
+
 
 class Shift(models.Model):
     id = models.AutoField(primary_key=True)
@@ -113,7 +130,8 @@ class Shift(models.Model):
     time_end = models.TimeField()
 
     class Meta:
-        unique_together = ('number','date')
+        unique_together = ('number', 'date')
+
 
 class Timetable(models.Model):
     id = models.AutoField(primary_key=True)
