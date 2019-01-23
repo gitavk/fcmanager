4c8a515a6960fde4df6201ca1e5a4d54c4749aeb
avk <avk@alarstudios.com>
2019-01-23 18:59:36 +0100

    Refactor code with pep8.

--- contract/views.py
+++ contract/views.py
@@ -4,9 +4,8 @@ from time import strptime
 import json
 
 from django.core.context_processors import csrf
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
--- fclub/settings.py
+++ fclub/settings.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-FCLUB_NAME = 'test'
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: don't run with debug turned on in production!
